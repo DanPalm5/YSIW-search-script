e1d5cc465f46d1c3cd0677f7d50fa1b34c79dd90
Daniel Palmieri <57024625+DanPalm5@users.noreply.github.com>
2019-11-14 21:01:46 -0500

    added 2 search tearms to the script

--- searchScript.py
+++ searchScript.py
@@ -1,7 +1,7 @@
 import requests
 import bs4
 url = 'https://arc.lib.montana.edu/ojs/index.php/Young-Scholars-In-Writing/search/search'
-searchTerms = ["meltin", "melting", "rhetori", "rhetoric"]  # the terms we want to search for
+searchTerms = ["meltin", "melting", "rhetori", "rhetoric", "justice", "justic"]  # the terms we want to search for
 obj = []   # creates a dictionary of terms to pass to query
 toRetrieve = []  # stores requests sent to website via POST
 instance = []  # each instance of a request is parsed into a string
@@ -13,3 +13,4 @@ for i in range(len(searchTerms)):
     instance.append(bs4.BeautifulSoup(toRetrieve[i].text, 'html.parser'))
     results.append(instance[i].findAll('div', {'class': 'obj_article_summary', 'class': 'title'}))
     print('Results for', searchTerms[i], ':', results[i])  # shows us the results of search
+    print("")
